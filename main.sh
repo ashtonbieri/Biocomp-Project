01db6f6b0d813f80ebecb6dc236456b269dc6dab
Ashton Bieri <abieri@crcfe01.crc.nd.edu>
2021-10-11 12:01:12 -0400

    Muscle, hmmer search profile, checks proteomes for mcrA presence and outputs only matches

---
+++ main.sh
@@ -0,0 +1,32 @@
+cd ~/Private/Biocomp-Project
+
+# Step 1: Combine fasta sequences into a .refs file 
+cat ./ref_sequences/hsp* > hsp70.refs
+cat ./ref_sequences/mcrA* > mcrA.refs
+
+# Step 2: Align sequences with hammer
+~/muscle -in hsp70.refs -out hsp70aligned.refs
+~/muscle -in mcrA.refs -out mcrA.refs
+
+# Step 3: Build profile hmm ??
+~/Private/hmmer/bin/hmmbuild hmmhsp70.txt hsp70aligned.refs
+~/Private/hmmer/bin/hmmbuild hmmmcrA.txt mcrA.refs
+
+# Step 4: Search proteomes based on matches
+# for proteome in [all proteomes]; 
+#   search for mcrA presence
+#   search for several hsp70 genes
+
+#search for mcrA matches in each proteome ; add each filename & # of possible matches
+#to mcrAsearch.txt ; 
+rm mcrAsearch.txt
+for pro in proteomes/*.fasta
+do
+~/Private/hmmer/bin/hmmsearch --tblout tempsearch.txt hmmmcrA.txt $pro
+echo -n $pro ' ' >> mcrAsearch.txt
+cat tempsearch.txt | grep -v "#" | wc -l >> mcrAsearch.txt
+done
+
+#get a list of proteomes that have =/ 0 matches
+cat mcrAsearch.txt | grep -v " 0" | cut --delimiter=' ' -f 1 > mcrAsearch.txt
+
