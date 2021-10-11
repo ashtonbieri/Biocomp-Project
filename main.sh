49fb8c94a15fc9dee64e28dc88455af8de242098
Ashton Bieri <abieri@crcfe01.crc.nd.edu>
2021-10-11 13:37:44 -0400

    Outputs all proteomes in table format, and all matches sorted by hsp70 matches

--- main.sh
+++ main.sh
@@ -23,22 +23,34 @@ rm mcrAsearch.txt
 for pro in proteomes/*.fasta
 do
 ~/Private/hmmer/bin/hmmsearch --tblout tempsearch.txt hmmmcrA.txt $pro
-echo -n $pro ' ' >> mcrAsearch.txt
+echo -n "$pro " >> mcrAsearch.txt
 cat tempsearch.txt | grep -v "#" | wc -l >> mcrAsearch.txt
 done
 
 #get a list of proteomes that have =/ 0 matches ; outputted in format of relative filepath
 cat mcrAsearch.txt | grep -v " 0" | cut --delimiter=' ' -f 1 > mcrAmatches.txt
 
-#Step 5: Search mcrA-matching proteomes based on matching for hsp70
+#Step 5: Search proteomes based on matching for hsp70
 
 rm hsp70search.txt
-for pro in $(cat mcrAmatches.txt)
+for pro in proteomes/*.fasta
 do
 ~/Private/hmmer/bin/hmmsearch --tblout tempsearch1.txt hmmhsp70.txt $pro
-echo -n $pro ' ' >> hsp70search.txt
+echo -n "$pro " >> hsp70search.txt
 cat tempsearch1.txt | grep -v "#" | wc -l >> hsp70search.txt
 done
 
-#Step 6: Organize results based on hsp presence, amount
-cat hsp70search.txt | grep -v " 0" | sort -k -r 2 > matches.txt
+echo -e 'Proteome\t\t\tmcrA matches\thsp70 matches' > table.txt
+for pro in proteomes/*.fasta
+do
+echo -e -n $pro'\t' >> table.txt
+mcrA=$(grep "$pro" mcrAsearch.txt | cut --delimiter=' ' -f 2)
+hsp70=$(grep "$pro" hsp70search.txt | cut --delimiter=' ' -f 2)
+echo "$mcrA               $hsp70" >> table.txt
+done
+
+cat table.txt | grep -v "matches" | grep -v "[[:space:]]0" | sort -k 3 -r > matches.txt
+echo -e '\n\n\n\nAll Proteomes (available in table.txt):'
+cat table.txt
+echo -e '\n\nPossible matches, sorted with strongest matches first(available in matches.txt):'
+cat matches.txt
