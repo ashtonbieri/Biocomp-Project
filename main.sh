55deaee21ac87e7afb5fbd3fa63e889e4658fe24
Ashton Bieri <abieri@crcfe01.crc.nd.edu>
2021-10-11 12:36:20 -0400

    Completed rough draft of assignment; outputs all mcrA-positive files sorted by number of hsp70 matches > 0

--- main.sh
+++ main.sh
@@ -12,13 +12,13 @@ cat ./ref_sequences/mcrA* > mcrA.refs
 ~/Private/hmmer/bin/hmmbuild hmmhsp70.txt hsp70aligned.refs
 ~/Private/hmmer/bin/hmmbuild hmmmcrA.txt mcrA.refs
 
-# Step 4: Search proteomes based on matches
+# Step 4: Search proteomes based on mcrA match
 # for proteome in [all proteomes]; 
 #   search for mcrA presence
-#   search for several hsp70 genes
 
 #search for mcrA matches in each proteome ; add each filename & # of possible matches
 #to mcrAsearch.txt ; 
+
 rm mcrAsearch.txt
 for pro in proteomes/*.fasta
 do
@@ -28,5 +28,17 @@ cat tempsearch.txt | grep -v "#" | wc -l >> mcrAsearch.txt
 done
 
 #get a list of proteomes that have =/ 0 matches ; outputted in format of relative filepath
-cat mcrAsearch.txt | grep -v " 0" | cut --delimiter=' ' -f 1 > mcrAsearch.txt
+cat mcrAsearch.txt | grep -v " 0" | cut --delimiter=' ' -f 1 > mcrAmatches.txt
 
+#Step 5: Search mcrA-matching proteomes based on matching for hsp70
+
+rm hsp70search.txt
+for pro in $(cat mcrAmatches.txt)
+do
+~/Private/hmmer/bin/hmmsearch --tblout tempsearch1.txt hmmhsp70.txt $pro
+echo -n $pro ' ' >> hsp70search.txt
+cat tempsearch1.txt | grep -v "#" | wc -l >> hsp70search.txt
+done
+
+#Step 6: Organize results based on hsp presence, amount
+cat hsp70search.txt | grep -v " 0" | sort -k -r 2 > matches.txt
