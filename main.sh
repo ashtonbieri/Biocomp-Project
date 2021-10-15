3709279a0bf7567ab5d29c2c07479af936b764ca
Ashton Bieri <abieri@crcfe01.crc.nd.edu>
2021-10-15 11:58:17 -0400

    Final edits for submission

--- main.sh
+++ main.sh
@@ -1,23 +1,29 @@
+#Code for Biocomp-Project for Intro to Biocomputing, BIOS-30318
+#Authored solely by Ashton Bieri, netid abieri, email abieri@nd.edu
+
+#Usage from command line:  >bash ./main.sh
+#Uses ref sequences from ./ref_sequences with filenames starting with hsp* and mcrA*
+#To run on another machine, edit lines(inc. blank lines) 16 and 17 so that ~/muscle represents the muscle program on your system,
+#    change lines 21 and 22 with the path to hmmbuild on your system, and lines 31 and 44 to the path to hmmsearch
+
 cd ~/Private/Biocomp-Project
 
 # Step 1: Combine fasta sequences into a .refs file 
-cat ./ref_sequences/hsp* > hsp70.refs
+cat ./ref_sequences/hsp* > hsp70.refs 
 cat ./ref_sequences/mcrA* > mcrA.refs
 
 # Step 2: Align sequences with hammer
 ~/muscle -in hsp70.refs -out hsp70aligned.refs
-~/muscle -in mcrA.refs -out mcrA.refs
+~/muscle -in mcrA.refs -out mcrAaligned.refs
 
 # Step 3: Build profile hmm ??
+#    uses hsp70aligned.refs, mcrAaligned.refs to make hmmhsp70.txt, hmmmcrA.txt hmmprofiles
 ~/Private/hmmer/bin/hmmbuild hmmhsp70.txt hsp70aligned.refs
-~/Private/hmmer/bin/hmmbuild hmmmcrA.txt mcrA.refs
+~/Private/hmmer/bin/hmmbuild hmmmcrA.txt mcrAaligned.refs
 
 # Step 4: Search proteomes based on mcrA match
-# for proteome in [all proteomes]; 
-#   search for mcrA presence
-
-#search for mcrA matches in each proteome ; add each filename & # of possible matches
-#to mcrAsearch.txt ; 
+#    add each filename & # of possible matches to mcrAsearch.txt
+#    # of possible matches are found by counting the number of match output lines (lines not starting with #)
 
 rm mcrAsearch.txt
 for pro in proteomes/*.fasta
@@ -40,6 +46,8 @@ echo -n "$pro " >> hsp70search.txt
 cat tempsearch1.txt | grep -v "#" | wc -l >> hsp70search.txt
 done
 
+
+#do the same thing for hsp70, set up formatting for table output
 echo -e 'Proteome\t\t\tmcrA matches\thsp70 matches' > table.txt
 for pro in proteomes/*.fasta
 do
@@ -49,7 +57,8 @@ hsp70=$(grep "$pro" hsp70search.txt | cut --delimiter=' ' -f 2)
 echo "$mcrA               $hsp70" >> table.txt
 done
 
-cat table.txt | grep -v "matches" | grep -v "[[:space:]]0" | sort -k 3 -r > matches.txt
+#cuts out all of the rows w/ a 0 for hsp70/mcrA, outputs to file and stdout
+cat table.txt | grep -v "matches" | grep -v "[[:space:]][0]" | sort -k 3 -r > matches.txt
 echo -e '\n\n\n\nAll Proteomes (available in table.txt):'
 cat table.txt
 echo -e '\n\nPossible matches, sorted with strongest matches first(available in matches.txt):'
